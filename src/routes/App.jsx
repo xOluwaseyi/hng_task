ebb2c152cd191b907a64ba9906aa5c88d9270045
Fagbemi Oluwaseyi <seyifagbemi211@gmail.com>
2022-11-03 17:23:49 +0100

    style: added styling to contact page

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -1,18 +1,17 @@
 import React from "react";
-import classes from "./App.module.css"
-import { Outlet } from "react-router-dom";
+import classes from "./App.module.css";
 import FooterSection from "../components/FooterSection/FooterSection";
 import LinksSection from "../components/LinksSection/LinksSection";
 import ProfileSection from "../components/ProfileSection/ProfileSection";
 
 const App = () => {
   return (
-    <main className={classes.app_container}>
-      <ProfileSection />
-      <LinksSection />
+    <main>
+      <section className={classes.app_container}>
+        <ProfileSection />
+        <LinksSection />
+      </section>
       <FooterSection />
-
-      <Outlet />
     </main>
   );
 };
--- src/routes/Contact/ContactPage.jsx
+++ src/routes/Contact/ContactPage.jsx
@@ -1,63 +1,135 @@
-import React from "react";
+import React, { useState } from "react";
 import classes from "./ContactPage.module.css";
 import FooterSection from "../../components/FooterSection/FooterSection";
 
 const ContactPage = () => {
+  const [formData, setFormData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    message: "",
+  });
+
+  const [emailError, setEmailError] = useState(false);
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const { firstName, lastName, email, message } = formData;
+
+    if (message === "") {
+      setEmailError(true);
+    }
+
+    if (firstName !== "" && lastName !== "" && email !== "" && message !== "") {
+      setEmailError(false);
+      setFormData({
+        firstName: "",
+        lastName: "",
+        email: "",
+        message: "",
+      });
+    }
+  };
+
   return (
-    <section className={classes.contact_container}>
-      <h1>Contact Me</h1>
-      <p>Hi there, contact me to ask me about anything you have in mind.</p>
+    <section>
+      <div className={classes.contact_container}>
+        <h1>Contact Me</h1>
+        <p>Hi there, contact me to ask me about anything you have in mind.</p>
 
-      <form action="">
-        {/* first name and last name */}
-        <div>
-          {/* first name */}
-          <div>
-            <label htmlFor="first_name">First Name</label>
+        <form
+          action=""
+          onSubmit={handleSubmit}
+          className={classes.contact_form}
+        >
+          {/* first name and last name */}
+          <div className={classes.form_names}>
+            {/* first name */}
+            <div>
+              <label htmlFor="first_name">First Name</label>
+              <input
+                value={formData.firstName}
+                onChange={handleInputChange}
+                type="text"
+                id="first_name"
+                name="firstName"
+                placeholder="Enter your first name"
+                required
+              />
+            </div>
+
+            {/* last name */}
+            <div>
+              <label htmlFor="last_name">Last Name</label>
+              <input
+                value={formData.lastName}
+                onChange={handleInputChange}
+                type="text"
+                id="last_name"
+                name="lastName"
+                placeholder="Enter your last name"
+                required
+              />
+            </div>
+          </div>
+
+          {/* email */}
+          <div className={classes.form_email}>
+            <label htmlFor="email">Email</label>
             <input
-              type="text"
-              id="first_name"
-              placeholder="Enter your first name"
+              value={formData.email}
+              onChange={handleInputChange}
+              type="email"
+              id="email"
+              name="email"
+              placeholder="yourname@email.com"
+              required
             />
           </div>
 
-          {/* last name */}
-          <div>
-            <label htmlFor="last_name">Last Name</label>
-            <input
-              type="text"
-              id="last_name"
-              placeholder="Enter your last name"
-            />
+          {/* message */}
+          <div className={classes.form_message}>
+            <label htmlFor="message">Message</label>
+            <textarea
+              style={{
+                outline: `${
+                  emailError ? "2px solid red" : "2px solid #d0d5dd"
+                }`,
+              }}
+              value={formData.message}
+              onChange={handleInputChange}
+              id="message"
+              name="message"
+              placeholder="Send me a message and I'll reply you as soon as possible..."
+              maxLength="5"
+            ></textarea>
+            {emailError && <p>Please enter a message</p>}
           </div>
-        </div>
 
-        {/* email */}
-        <label htmlFor="email">Email</label>
-        <input type="email" id="email" placeholder="yourname@email.com" />
+          {/* confirmation */}
+          <div className={classes.form_confirm}>
+            <input type="checkbox" id="confirm" />
+            <p>
+              You agree to providing your data to Oluwaseyi who may contact you.
+            </p>
+          </div>
 
-        {/* message */}
-        <label htmlFor="message">Message</label>
-        <textarea
-          id="message"
-          cols="30"
-          rows="10"
-          placeholder="Send me a message and I'll reply you as soon as possible..."
-        ></textarea>
-
-        {/* confirmation */}
-        <div>
-          <input type="checkbox" id="confirm" />
-          <span>
-            You agree to providing your data to {name} who may contact you.
-          </span>
-        </div>
-
-        {/* submit */}
-        <button type="submit" id="btn__submit">
-          Submit
-        </button>
-      </form>
+          {/* submit */}
+          <button type="submit" id="btn__submit">
+            Send message
+          </button>
+        </form>
+      </div>
 
       <FooterSection />
     </section>
